a8a3bb02ce0c4560a4feb6ed9ec9dddd9e26626c
rushi <rushi44@gmail.com>
2021-03-18 21:05:27 -0700

    show current value

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import React from "react";
-import { useMachine } from "@xstate/react";
-import { Machine, send } from "xstate";
+import React from 'react'
+import { useMachine } from '@xstate/react'
+import { Machine, send } from 'xstate'
 
 const toggleMachine = Machine({
-  id: "toggleMachine",
-  initial: "inactive",
+  id: 'toggleMachine',
+  initial: 'inactive',
   states: {
     inactive: {
       on: {
-        TOGGLE: "active",
+        TOGGLE: 'active',
       },
     },
     active: {
       on: {
-        TOGGLE: "inactive",
+        TOGGLE: 'inactive',
       },
     },
   },
-});
+})
 
 export const App = () => {
-  const [current, send] = useMachine(toggleMachine);
+  const [current, send] = useMachine(toggleMachine)
 
-  console.log(current.value);
+  console.log(current.value)
 
   return (
     <div className="App">
-      <button onClick={() => send("TOGGLE")}>Toggle</button>
+      <button onClick={() => send('TOGGLE')}>Toggle</button>
+      <p>We are {current.value}</p>
     </div>
-  );
-};
+  )
+}
